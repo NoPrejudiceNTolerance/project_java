3e2bd728a2c9d5975c0e1443912d0930af88cada
NoPrejudiceNTolerance <antoine.malinet@orange.fr>
2017-11-22 17:09:20 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Lolcation/src/principal/Emprunteur.java

--- Lolcation/src/principal/Emprunteur.java
+++ Lolcation/src/principal/Emprunteur.java
@@ -8,7 +8,16 @@ class Emprunteur {
 
   private Adresse adresse;
 
+<<<<<<< Updated upstream
   public void louer() {
+=======
+<<<<<<< HEAD
+  public  louer() {
+	  // Louis tu as vrmt un petit sexe (11 cm)
+=======
+  public void louer() {
+>>>>>>> origin/master
+>>>>>>> Stashed changes
   }
 
   public void  ramener() {
